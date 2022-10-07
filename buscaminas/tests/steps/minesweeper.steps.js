c7bc5ee46ca0851499d0916a28ca8bc426004c12
Èric <50328668+EricBM19@users.noreply.github.com>
2022-10-07 10:45:20 +0200

    Tests and steps folder
    Includes the first test Given the user opens the app

---
+++ buscaminas/tests/steps/minesweeper.steps.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { expect } = require('@playwright/test');
+
+const url = 'http://127.0.0.1:5500/main.html';
+
+Given('a user opens the app', async () => {
+	await page.goto(url);
+});
