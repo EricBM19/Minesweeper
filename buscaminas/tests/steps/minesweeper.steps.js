e54cc1e096a57d7e069096769d242b7163ec0825
Èric <50328668+EricBM19@users.noreply.github.com>
2022-10-20 13:36:02 +0200

    Check Board dimensions Step

--- buscaminas/tests/steps/minesweeper.steps.js
+++ buscaminas/tests/steps/minesweeper.steps.js
@@ -26,3 +26,19 @@ Then('the status should be: {string}', async (string) =>
   let status = await page.locator('data-test-id=status').innerText();
   expect(status).toBe(string);
 });
+
+Then('the amount of rows should be: {int}', async (int) => 
+{
+  let board = await page.locator('data-test-id=minefield').locator('div');
+  let lastCell = await board.last().getAttribute('id');
+  let rowId = await lastCell.split("-")[0];
+  expect(parseInt(rowId)).toBe(int-1);
+});
+
+Then('the amount of columns should be: {int}', async (int) => 
+{
+  let board = await page.locator('data-test-id=minefield').locator('div');
+  let lastCell = await board.last().getAttribute('id');
+  let colId = await lastCell.split("-")[1];
+  expect(parseInt(colId)).toBe(int-1);
+});
