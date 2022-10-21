e1809bcadf86e60e69411c20f1ccaced5aeac040
Èric <50328668+EricBM19@users.noreply.github.com>
2022-10-21 11:27:03 +0200

    Url correction

--- buscaminas/tests/steps/minesweeper.steps.js
+++ buscaminas/tests/steps/minesweeper.steps.js
@@ -2,7 +2,7 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
 
-const url = 'http://127.0.0.1:5500/main.html';
+const url = 'http://127.0.0.1:5500/index.html';
 
 Given('a user opens the app', async () => 
 {
